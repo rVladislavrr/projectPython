a4be93d74fbbd164ded3226744237e1a5a2d3407
rVladislavrr <rvladislavrr@mail.ru>
2024-01-23 16:58:41 +0500

    Get запрос с фронта на описание и скиллы И правки

--- project/views.py
+++ project/views.py
@@ -13,7 +13,6 @@ cxt_new_vac = {
 
 
 def reqvest():
-
     def fetch_vacancies():
         keywords = ['fullstack', 'фулстак', 'фуллтак', 'фуллстэк', 'фулстэк', 'full stack']
         today = datetime.now().replace(hour=0, minute=0, second=0)
@@ -31,6 +30,7 @@ def reqvest():
 
         for vacancy in vacancies:
             vacancy_data = {
+                'id': vacancy["id"],
                 "name": vacancy["name"],
                 "employer_name": vacancy["employer"]["name"],
                 "salary": vacancy.get("salary", ""),
@@ -38,40 +38,16 @@ def reqvest():
                 "published_at": vacancy["published_at"].replace('T', ' ')[:19] + ' (по Мск)',
                 'url': f"https://hh.ru/vacancy/{vacancy["id"]}"
             }
-
-            description, skills = get_vacancy_details(vacancy["id"])
-            description_sh = description[: description.find(';' , description.find(';') + 1 )] + '...'
-            description_ds = description[: description.find('.' , description.find('.') + 1 )] + '...'
-
-            vacancy_data["description"] = description_sh if len(description_sh) < len(description_ds) else description_ds
-            vacancy_data["skills"] = skills
-
             result.append(vacancy_data)
-
         return result
-
-    def get_vacancy_details(vacancy_id):
-        url = f"https://api.hh.ru/vacancies/{vacancy_id}"
-        response = requests.get(url)
-        data = response.json()
-
-        description = data.get("description", "")
-
-        skills = ""
-        if "key_skills" in data and data["key_skills"]:
-            skills = ", ".join([skill["name"] for skill in data["key_skills"]])
-
-        return remove_html_tags(description), skills
-
-    def remove_html_tags(text):
-        soup = BeautifulSoup(text, "html.parser")
-        cleaned_text = soup.get_text()
-        return cleaned_text
-
     vacancies = fetch_vacancies()
     dict_ = {'vac': vacancies}
-    global cxt_new_vac
-    cxt_new_vac = dict_
+    return dict_
+
+def remove_html_tags(text):
+    soup = BeautifulSoup(text, "html.parser")
+    cleaned_text = soup.get_text()
+    return cleaned_text
 
 
 def year_salar():
@@ -156,7 +132,30 @@ def page_4(request):
 
 
 def page_5(request):
-    t1 = threading.Thread(target=reqvest)
-    t1.start()
-    global cxt_new_vac
+    cxt_new_vac = reqvest()
     return render(request, 'new_vac.html', context=cxt_new_vac)
+
+
+from django.http import JsonResponse
+
+
+def get_additional_info(request):
+    card_id = request.GET.get('card_id')
+
+    url = f"https://api.hh.ru/vacancies/{card_id}"
+    response = requests.get(url)
+    data = response.json()
+
+    description = remove_html_tags(data.get("description", ""))
+
+    skills = ""
+    if "key_skills" in data and data["key_skills"]:
+        skills = ", ".join([skill["name"] for skill in data["key_skills"]])
+
+    description_sh = description[: description.find(';', description.find(';') + 1)] + '...'
+    description_ds = description[: description.find('.', description.find('.') + 1)] + '...'
+
+    description = description_sh if len(description_sh) < len(description_ds) else description_ds
+    if not skills:
+        skills = 'отсутствуют'
+    return JsonResponse({'des': description, 'skills': skills})
